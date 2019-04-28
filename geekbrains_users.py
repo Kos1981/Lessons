f8d20e7db3ebe220326f905f414f0f75279d4bf1
Kos1981 <babarykink96@gmail.com>
2019-04-29 03:43:48 +0400

    Упражнение к уроку 2 ООП (получение кол-ва пользователей с сайта geekbrains)

---
+++ geekbrains_users.py
@@ -0,0 +1,26 @@
+﻿#1. Получить количество учеников с сайта geekbrains.ru:
+#a) при помощи регулярных выражений,
+#b) при помощи библиотеки BeautifulSoup.
+
+
+import re
+from bs4 import BeautifulSoup as BS
+
+
+def get_with_BS():
+    with open("c:\index1.html", encoding='utf-8') as f:
+         s=f.read()
+
+    soup = BS(s,"html.parser")
+    span_tags=soup.find_all("span")
+    print(span_tags[0].string)
+
+
+def get_with_re():
+    with open("c:\index1.html", encoding='utf-8') as f:
+        s=f.read()
+    li=re.findall("Нас уже \d \d+ \d+",s)
+    print(li)
+
+get_with_BS()
+get_with_re()
